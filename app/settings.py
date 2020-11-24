c6f0518c49e961c7716d38533a5d0fef7af4e2b7
Robert, Drew <ddrobe2@ilstu.edu>
2020-11-23 21:13:01 -0600

    update static file route

--- app/settings.py
+++ app/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/redcycle-api/static/'
+STATIC_URL = '/redcycle/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
