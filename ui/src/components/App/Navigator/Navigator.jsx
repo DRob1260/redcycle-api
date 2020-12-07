2fef4df2e13cd8d511cd35a8e56bd25f6aaaf661
Hunter <hunterberry640@gmail.com>
2020-12-06 21:24:53 -0600

    added login to nav and routed it to login page

--- ui/src/components/App/Navigator/Navigator.jsx
+++ ui/src/components/App/Navigator/Navigator.jsx
@@ -13,9 +13,13 @@ const Navigator = () => {
           </Link>
         </Navbar.Brand>
         <Navbar.Collapse>
-          <Nav>
+          <Nav className="mr-auto">
             <Nav.Link><Link to={'/redcycle'}>Home</Link></Nav.Link>
             <Nav.Link><Link to={'/redcycle/post-creator'}>Post Creator</Link></Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav><a target="_blank" href="http://127.0.0.1:8000/redcycle/api/account/login/">Log in</a>
+            </Nav>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
