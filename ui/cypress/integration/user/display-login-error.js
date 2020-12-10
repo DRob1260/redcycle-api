5c9fdcf98a9cfa5766a3ba4faf8a793dd84ee1a5
drewrobert <drew.robert26@me.com>
2020-12-10 07:36:17 -0600

    fix tests

--- ui/cypress/integration/user/display-login-error.js
+++ ui/cypress/integration/user/display-login-error.js
@@ -1,10 +1,11 @@
 context('display login error', () => {
     it('can display incorrect login info error', () => {
-      cy.visit('/redcycle/api/account/signin');
-      cy.get('#id_email').type("Account that doesn't exist");
-      cy.get('#id_password').type("Fake password");
-      cy.get('button').click();
-      cy.url().should('include', '/login')
-      cy.getCookie('sessionid').should('not.exist')
+          cy.visit('/redcycle/api/account/signin');
+          cy.get('#id_email').type("nonexistantaccount@gmail.com");
+          cy.get('#id_password').type("Fake password");
+          cy.get('button').click();
+          cy.url().should('include', '/signin')
+          cy.getCookie('sessionid').should('not.exist')
+          cy.contains("This user does not exists").should("exist");
     });
 });
--- ui/cypress/integration/user/navigate-to-post-creation-page.js
+++ ui/cypress/integration/user/navigate-to-post-creation-page.js
@@ -1,9 +1,9 @@
 context('navigate to post creation page', () => {
-  it("navigates to the post creation page when the 'Post Creator' button is clicked", () => {
+  it("navigates to the login page when the 'Post Creator' button is clicked and the user is not logged in", () => {
     cy.visit('/redcycle');
     cy.get('.Home').should('be.visible');
     cy.get('.PostCreator').should('not.be.visible');
     cy.get('[href="/redcycle/post-creator"]').click();
-    cy.get('.PostCreator').should('be.visible');
+    cy.contains('Login');
   });
 });
