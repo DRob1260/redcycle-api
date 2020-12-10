d2eff195f0256c85ca30cd37f36439f3f8495c2b
drewrobert <drew.robert26@me.com>
2020-12-09 21:48:58 -0600

    fix routing issue

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('redcycle/admin/', admin.site.urls),
     path('redcycle/api', include('redcycle.urls')),
-    path('redcycle/', include('ui.urls'))
+    path('redcycle', include('ui.urls')),
 ]
